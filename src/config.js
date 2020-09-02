260f0def8a591f78829401f29adf9696868584de
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-02 13:34:37 -0400

    adjusted.env files

--- src/config.js
+++ src/config.js
@@ -6,3 +6,4 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
+//password: 14eb10b5fc5089a0914daf2aa5aeb8ca42cf81edbb61e96900881f25bac391d2
